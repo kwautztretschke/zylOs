d1949066770eefb7c2db59fcad990f757e9985eb
Victoria Stöffler <victoria.stoeffler@tum.de>
2023-09-26 06:07:36 +0200

    added artnetprogram class and program utility functions

--- include/Program.h
+++ include/Program.h
@@ -6,13 +6,22 @@
 #include "ZylOsConfig.h"
 #include "ProgramManager.h"
 
+#define ARTNET_HISTORY_SIZE 256
+
 
 class Program{
-public: //TODO make some of these Private?
+protected:
+	//* utility functions you could use
+	int 					m_ColorIndex = 0;
+						// returns the color at the current index
+	CRGB					getColor();
+						// returns the color relative to the current index
+	CRGB					getColorRelative(int index);
+public: 
+	//* necessary stuff for the program manager
 	const char*				m_Name;
 	CRGB					m_FB[FB_SIZE] = {0};
 	Program*				m_pNext = 0;
-	//* methods applicable for each Program
 	;						Program();
 	;						Program(bool add);
 	//* methods to be redefined by programs -> "interface"
@@ -22,6 +31,33 @@ public: //TODO make some of these Private?
 	virtual int				input(char* key, char* value){return 0;}
 	virtual void			artnet(const uint8_t* data, const uint16_t size){};
 };
+
+class ArtnetProgram : public Program{
+protected:
+	int 					m_Channel = 0;
+	int 					m_Threshold = 0;
+	int 					m_Smooth = 0;
+	char* 					m_pArtNetHistory = nullptr;
+	int 					m_ArtNetHistorySize = 0;
+	int						m_ArtNetHistoryIndex = 0;
+	//* artnet utility functions
+	uint8_t					getArtNetHistory(int index);
+						// returns the color at the current index, 
+						// modulated by the artnet history (with smoothing)
+	CRGB					getModulatedColor();
+	CRGB					getModulatedColorRelative(int index);
+						// gets called after the regular input function, for your own needs
+	virtual int				inputFurther(char* key, char* value){return 0;} 
+						// gets called after the regular artnet function, for your own needs
+	virtual void			artnetFurther(const uint8_t* data, const uint16_t size){};
+public:
+	using Program::Program;
+						// handles channel and threshold, if that's all you need
+	virtual int 			input(char* key, char* value) override;
+						// puts the selected channel into the history
+	virtual void			artnet(const uint8_t* data, const uint16_t size) override;
+};
+
 
 
 #endif // __PROGRAM_H__
--- src/Program.cpp
+++ src/Program.cpp
@@ -11,3 +11,69 @@ Program::Program(bool add)
 	if(add)
 		ProgramManager::add(this);
 }
+
+CRGB Program::getColor()
+{	// returns the color at the current index
+	return ProgramManager::getColor(m_ColorIndex);
+}
+
+CRGB Program::getColorRelative(int index)
+{	
+	if (index == 0)
+		return ProgramManager::getColor(m_ColorIndex);
+	if (index < 0) {
+		// add enough multiples of ZPM_COLORS to make the index positive
+		index += (-index / ZPM_COLORS + 1) * ZPM_COLORS;
+	}
+	return ProgramManager::getColor((index+m_ColorIndex)%ZPM_COLORS);
+}
+
+int ArtnetProgram::input(char* key, char* value)
+{	// handles channel and threshold, if that's all you need
+	if(!strcmp(key, "channel"))
+		m_Channel = strtol(value, NULL, 10);
+	else if(!strcmp(key, "threshold"))	
+		m_Threshold = strtol(value, NULL, 10);
+	else if(!strcmp(key, "smooth"))	
+		m_Smooth = strtol(value, NULL, 10);
+	return inputFurther(key, value);
+}
+
+void ArtnetProgram::artnet(const uint8_t* data, const uint16_t size)
+{	// puts the selected channel into the history
+	if(m_Channel >= size)
+		return;
+	if (m_ArtNetHistoryIndex-- < 0)
+		m_ArtNetHistoryIndex = m_ArtNetHistorySize - 1;
+	m_pArtNetHistory[m_ArtNetHistoryIndex] = data[m_Channel];
+	artnetFurther(data, size);
+}
+
+uint8_t ArtnetProgram::getArtNetHistory(int index)
+{	
+	if (index < 0)
+	return 0;
+	
+	index = (m_ArtNetHistoryIndex + index) % m_ArtNetHistorySize;
+	return m_pArtNetHistory[index];
+}
+
+CRGB ArtnetProgram::getModulatedColor()
+{	// returns the color at the current index, 
+	// modulated by the artnet history (with smoothing)
+	return getModulatedColorRelative(0);
+}
+
+CRGB ArtnetProgram::getModulatedColorRelative(int index)
+{	// returns the color relative to the current index, 
+	// modulated by the artnet history (with smoothing)
+	CRGB c = getColorRelative(index);
+	if (m_Smooth == 0)
+		return c.nscale8(getArtNetHistory(0));
+
+	int sum = 0;
+	for (int i = 0; i < m_Smooth; i++)
+		sum += getArtNetHistory(i);
+	sum /= m_Smooth;
+	return c.nscale8(sum);
+}
